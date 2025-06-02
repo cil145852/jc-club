5acb81641780fffd4e0007f16b720dbb7f310711
liang <3409459141@qq.com>
2025-06-02 21:45:57 +0800

    创建公共返回结果对象

---
+++ jc-club-subject/jc-club-common/src/main/java/com/cjl/subject/common/entity/Result.java
@@ -0,0 +1,54 @@
+package com.cjl.subject.common.entity;
+
+import com.cjl.subject.common.enums.ResultCodeEnum;
+import lombok.Data;
+
+/**
+ * @author liang
+ * @version 1.0
+ * @CreateDate 2025-06-02-20:50
+ * @Description
+ */
+@Data
+public class Result<T> {
+    private Integer code;
+    private Boolean success;
+    private String message;
+    private T data;
+
+    public static Result ok() {
+        Result result = new Result<>();
+        ResultCodeEnum  successCode = ResultCodeEnum.SUCCESS;
+        result.setCode(successCode.getCode());
+        result.setSuccess(true);
+        result.setMessage(successCode.getDesc());
+        return result;
+    }
+    public static <T> Result<T> ok(T data) {
+        Result<T> result = new Result<>();
+        ResultCodeEnum  successCode = ResultCodeEnum.SUCCESS;
+        result.setCode(successCode.getCode());
+        result.setMessage(successCode.getDesc());
+        result.setSuccess(true);
+        result.setData(data);
+        return result;
+    }
+
+    public static Result fail() {
+        Result result = new Result<>();
+        ResultCodeEnum  failCode = ResultCodeEnum.FAIL;
+        result.setCode(failCode.getCode());
+        result.setSuccess(false);
+        result.setMessage(failCode.getDesc());
+        return result;
+    }
+    public static <T> Result<T> fail(T data) {
+        Result<T> result = new Result<>();
+        ResultCodeEnum  failCode = ResultCodeEnum.SUCCESS;
+        result.setCode(failCode.getCode());
+        result.setMessage(failCode.getDesc());
+        result.setSuccess(false);
+        result.setData(data);
+        return result;
+    }
+}
---
+++ jc-club-subject/jc-club-common/src/main/java/com/cjl/subject/common/enums/ResultCodeEnum.java
@@ -0,0 +1,33 @@
+package com.cjl.subject.common.enums;
+
+import lombok.Getter;
+
+/**
+ * @author liang
+ * @version 1.0
+ * @CreateDate 2025-06-02-20:54
+ * @Description
+ */
+@Getter
+public enum ResultCodeEnum {
+
+    SUCCESS(200, "成功"),
+    FAIL(500, "失败");
+
+    public Integer code;
+    public String desc;
+
+    ResultCodeEnum(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public static ResultCodeEnum getByCode(Integer code) {
+        for (ResultCodeEnum resultCodeEnum : ResultCodeEnum.values()) {
+            if (resultCodeEnum.code.equals(code)) {
+                return resultCodeEnum;
+            }
+        }
+        return null;
+    }
+}
