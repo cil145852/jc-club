ef22470ebc83872d46c0229342aaf955cd49ed2b
liang <3409459141@qq.com>
2025-06-03 20:14:47 +0800

    题目与分类标签关系表infra层实体类生成

---
+++ jc-club-subject/jc-club-infra/src/main/java/com/cjl/subject/infra/basic/entity/SubjectMapping.java
@@ -0,0 +1,54 @@
+package com.cjl.subject.infra.basic.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * 题目分类关系表(SubjectMapping)实体类
+ *
+ * @author makejava
+ * @since 2025-06-03 20:07:12
+ */
+@Data
+public class SubjectMapping implements Serializable {
+    private static final long serialVersionUID = -21219935695960794L;
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 题目id
+     */
+    private Long subjectId;
+    /**
+     * 分类id
+     */
+    private Long categoryId;
+    /**
+     * 标签id
+     */
+    private Long labelId;
+    /**
+     * 创建人
+     */
+    private String createdBy;
+    /**
+     * 创建时间
+     */
+    private Date createdTime;
+    /**
+     * 更新人
+     */
+    private String updateBy;
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+    /**
+     * 是否已删除 0否 1是
+     */
+    private Integer isDeleted;
+}
+
---
+++ jc-club-subject/jc-club-infra/src/main/java/com/cjl/subject/infra/basic/service/SubjectMappingService.java
@@ -0,0 +1,45 @@
+package com.cjl.subject.infra.basic.service;
+
+import com.cjl.subject.infra.basic.entity.SubjectMapping;
+
+/**
+ * 题目分类关系表(SubjectMapping)表服务接口
+ *
+ * @author makejava
+ * @since 2025-06-03 20:07:12
+ */
+public interface SubjectMappingService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    SubjectMapping queryById(Long id);
+
+    /**
+     * 新增数据
+     *
+     * @param subjectMapping 实例对象
+     * @return 实例对象
+     */
+    Integer insert(SubjectMapping subjectMapping);
+
+    /**
+     * 修改数据
+     *
+     * @param subjectMapping 实例对象
+     * @return 实例对象
+     */
+    Integer update(SubjectMapping subjectMapping);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Long id);
+
+}
