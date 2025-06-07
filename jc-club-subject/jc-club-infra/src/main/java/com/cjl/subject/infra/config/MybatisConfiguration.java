744706fcd4fc4325a9602bca5a71dd0f9774e6cf
liang <3409459141@qq.com>
2025-06-07 11:04:51 +0800

    配置sql拦截器，输出sql日志信息

---
+++ jc-club-subject/jc-club-infra/src/main/java/com/cjl/subject/infra/config/MybatisConfiguration.java
@@ -0,0 +1,23 @@
+package com.cjl.subject.infra.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liang
+ * @version 1.0
+ * @CreateDate 2025-06-07-11:00
+ * @Description
+ */
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+
+}
