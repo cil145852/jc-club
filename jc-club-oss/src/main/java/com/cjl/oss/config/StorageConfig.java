f480da3473880d6ae5403e93231935c6398159a0
liang <3409459141@qq.com>
2025-06-10 11:11:53 +0800

    使用nacos实现动态配置

--- jc-club-oss/src/main/java/com/cjl/oss/config/StorageConfig.java
+++ jc-club-oss/src/main/java/com/cjl/oss/config/StorageConfig.java
@@ -3,6 +3,7 @@ package com.cjl.oss.config;
 import com.cjl.oss.adapter.MinioStorageAdapter;
 import com.cjl.oss.adapter.StorageAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +14,13 @@ import org.springframework.context.annotation.Configuration;
  * @Description
  */
 @Configuration
+@RefreshScope
 public class StorageConfig {
     @Value("${storage.service.type}")
     private String storageServiceType;
 
     @Bean
+    @RefreshScope
     public StorageAdapter storageService() {
         if ("minio".equals(storageServiceType)) {
             return new MinioStorageAdapter();
