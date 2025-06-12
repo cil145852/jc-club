d5e5908e6f27a0f453f4582caa14c1e42cefd6bc
liang <3409459141@qq.com>
2025-06-12 15:21:49 +0800

    redis工具类封装

--- jc-club-gateway/src/main/java/com/cjl/club/gateway/auth/StpInterfaceImpl.java
+++ jc-club-gateway/src/main/java/com/cjl/club/gateway/auth/StpInterfaceImpl.java
@@ -1,8 +1,10 @@
 package com.cjl.club.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.cjl.club.gateway.redis.RedisUtil;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -13,6 +15,10 @@ import java.util.List;
  */
 @Component
 public class StpInterfaceImpl implements StpInterface {
+    @Resource
+    private RedisUtil redisUtil;
+
+    private final String AUTH_PREFIX = "auth";
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         // 返回此 loginId 拥有的权限列表
