a43b9a698d23cbab3723fd7c527181e67f121a72
liang <3409459141@qq.com>
2025-06-13 21:07:38 +0800

    角色权限关联实体类生成

---
+++ jc-club-auth/jc-club-auth-infra/src/main/java/com/cjl/auth/infra/basic/service/AuthRolePermissionService.java
@@ -0,0 +1,45 @@
+package com.cjl.auth.infra.basic.service;
+
+import com.cjl.auth.infra.basic.entity.AuthRolePermission;
+
+/**
+ * 角色权限关联表(AuthRolePermission)表服务接口
+ *
+ * @author makejava
+ * @since 2025-06-13 21:00:50
+ */
+public interface AuthRolePermissionService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    AuthRolePermission queryById(Long id);
+
+    /**
+     * 新增数据
+     *
+     * @param authRolePermission 实例对象
+     * @return
+     */
+    Integer insert(AuthRolePermission authRolePermission);
+
+    /**
+     * 修改数据
+     *
+     * @param authRolePermission 实例对象
+     * @return
+     */
+    Integer update(AuthRolePermission authRolePermission);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Long id);
+
+}
---
+++ jc-club-auth/jc-club-auth-infra/src/main/java/com/cjl/auth/infra/basic/service/impl/AuthRolePermissionServiceImpl.java
@@ -0,0 +1,64 @@
+package com.cjl.auth.infra.basic.service.impl;
+
+import com.cjl.auth.infra.basic.entity.AuthRolePermission;
+import com.cjl.auth.infra.basic.mapper.AuthRolePermissionDao;
+import com.cjl.auth.infra.basic.service.AuthRolePermissionService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 角色权限关联表(AuthRolePermission)表服务实现类
+ *
+ * @author makejava
+ * @since 2025-06-13 21:00:50
+ */
+@Service("authRolePermissionService")
+public class AuthRolePermissionServiceImpl implements AuthRolePermissionService {
+    @Resource
+    private AuthRolePermissionDao authRolePermissionDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public AuthRolePermission queryById(Long id) {
+        return this.authRolePermissionDao.queryById(id);
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param authRolePermission 实例对象
+     * @return
+     */
+    @Override
+    public Integer insert(AuthRolePermission authRolePermission) {
+        return this.authRolePermissionDao.insert(authRolePermission);
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param authRolePermission 实例对象
+     * @return
+     */
+    @Override
+    public Integer update(AuthRolePermission authRolePermission) {
+        return this.authRolePermissionDao.update(authRolePermission);
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Long id) {
+        return this.authRolePermissionDao.deleteById(id) > 0;
+    }
+}
